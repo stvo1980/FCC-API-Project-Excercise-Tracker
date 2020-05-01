0766257d662e0d1d79fe5e55ba682b277f13a724
Glitch (fcc-exercise-tracker-p) <none>
2020-05-01 23:29:32 +0000

    🌱🏹 Checkpoint ./server.js:15407664/34

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
+const shortId = require('shortid')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
