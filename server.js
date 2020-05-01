1b0c2cbad9351ca9b73c40fb81aed2faf240267e
Glitch (fcc-exercise-tracker-p) <none>
2020-05-01 21:16:37 +0000

    🏞🏨 Checkpoint ./server.js:15407664/772

--- server.js
+++ server.js
@@ -44,6 +44,16 @@ app.get('/', (req, res) => {
 });
 
 
+app.post("/api/exercise/new-user", function(req,res){
+  res.json({username:req.body.username, _id:})
+
+})
+
+app.post("/new-user", function(req,res){
+  res.json({username: "kellld"})
+ // res.json({username: req.body.username})
+})
+
 
 
 
@@ -72,21 +82,9 @@ app.use((err, req, res, next) => {
     .send(errMessage)
 })
 
-//app.get("/api/exercise/new-user", function(req, res) {
- 
 
- // res.send({username:"kitikfat"});
-//})
 
-app.post("/new", function(req,res){
-  res.json({username: "kellld"})
- // res.json({username: req.body.username})
-})
 
-app.post("/new-user", function(req,res){
-  res.json({username: "kellld"})
- // res.json({username: req.body.username})
-})
 
 
 
