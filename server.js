8e01d5d34e5bdd3ce523e005dbd13ca2da207d93
Glitch (fcc-exercise-tracker-p) <none>
2020-05-02 15:47:34 +0000

    🐄📖 Checkpoint ./server.js:15407664/314

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const shortId = require('shortid')
 const cors = require('cors')
 var autoIncrement = require('mongoose-auto-increment')
 const mongoose = require('mongoose')
-//mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
-//.then(() => console.log("DB Connected!"))
-//  .catch(err => {
-//    console.log(`DB Connection Error: ${err.message}`);
-//  });
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+.then(() => console.log("DB Connected!"))
+  .catch(err => {
+    console.log(`DB Connection Error: ${err.message}`);
+  });
 
 
 //need to switch to shortId after finishing work
-const connection = mongoose.createConnection(process.env.MLAB_URI)
-autoIncrement.initialize(connection)
+//const connection = mongoose.createConnection(process.env.MLAB_URI)
+//autoIncrement.initialize(connection)
 
 
 app.use(cors())
@@ -28,6 +28,7 @@ const Schema=mongoose.Schema;
 const PersonSchema = new Schema ({
  // shortId: {type: String, unique: true, default: shortId.generate},
  username: { type: String, required: true, unique: true },
+  _id: { type: String, default: shortId.generate },
  exercise: [{
     desc : String,
     duration: Number,
@@ -35,11 +36,11 @@ const PersonSchema = new Schema ({
   }]
 });
 
-//var Person = mongoose.model('Person', PersonSchema); 
+var Person = mongoose.model('Person', PersonSchema); 
 
-PersonSchema.plugin(autoIncrement.plugin, 'Person')
+//PersonSchema.plugin(autoIncrement.plugin, 'Person')
 
-const Person = connection.model('Person', PersonSchema)
+//const Person = connection.model('Person', PersonSchema)
 
 
 
