2aa1b5a2de81ad48bf6f5a809dcb52f014b5140d
Glitch (fcc-exercise-tracker-p) <none>
2020-05-04 22:40:19 +0000

    🐲👩 Checkpoint ./server.js:15407664/61

--- server.js
+++ server.js
@@ -197,28 +197,28 @@ app.get("/api/exercise/log/", (req, res) => {
 
 
 // Not found middleware
-//app.use((req, res, next) => {
- // return next({status: 404, message: 'not found'})
-//})
+app.use((req, res, next) => {
+  return next({status: 404, message: 'not found'})
+})
 
-// Error Handling middleware
-//app.use((err, req, res, next) => {
-//  let errCode, errMessage
+ // Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
 
- // if (err.errors) {
-    // mongoose validation error
- //   errCode = 400 // bad request
- //   const keys = Object.keys(err.errors)
-    // report the first validation error
- //   errMessage = err.errors[keys[0]].message
- // } else {
-    // generic or custom error
-//    errCode = err.status || 500
- //   errMessage = err.message || 'Internal Server Error'
- // }
-//  res.status(errCode).type('txt')
-//    .send(errMessage)
-//})
+  if (err.errors) {
+   //  mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+ //    report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+//     generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
 
 
 
